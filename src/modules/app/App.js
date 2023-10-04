62966cd8913c728ecc33211c96a188e2589910c5
macbook <playegrovoy@gmail.com>
2023-10-04 19:01:56 +0300

    use payment api

---
+++ src/modules/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PaymentView from '../payment_view/PaymentView';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentView/>
+    </div>
+  );
+}
+
+export default App;
